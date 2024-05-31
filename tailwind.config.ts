bc06b24f17d0eb71e7dbca66d04e375ae0c35a9f
Adil-Mukhtar <adilmukhtar327@gmail.com>
2024-05-31 22:08:04 +0500

    changed color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,12 +12,13 @@ const config: Config = {
         '13': 'repeat(13, minmax(0, 1fr))',
       },
       colors: {
-        blue: {
-          400: '#2589FE',
-          500: '#0070F3',
-          600: '#2F6FEB',
+        peach: {
+          300: '#ffddcf',
+          400: '#FFB07C',
+          500: '#FF8C69',
+          600: '#FF6347',
         },
-      },
+      },      
     },
     keyframes: {
       shimmer: {
